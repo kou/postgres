6108e59963e6a87c6c8fb718160bd92a4458b7f3
Sutou Kouhei <kou@clear-code.com>
2024-07-23 17:39:41 +0900

    Add support for adding custom COPY TO/FROM format
    This uses the handler approach like tablesample. The approach creates
    an internal function that returns an internal struct. In this case,
    a COPY TO handler returns a CopyToRoutine and a COPY FROM handler
    returns a CopyFromRoutine.
    
    This uses the same handler for COPY TO and COPY FROM. PostgreSQL calls a
    COPY TO/FROM handler with "is_from" argument. It's true for COPY FROM
    and false for COPY TO:
    
        copy_handler(true) returns CopyToRoutine
        copy_handler(false) returns CopyFromRoutine
    
    This also add a test module for custom COPY TO/FROM handler.

--- src/include/commands/copy.h
+++ src/include/commands/copy.h
@@ -87,6 +87,8 @@ typedef struct CopyFormatOptions
 	CopyLogVerbosityChoice log_verbosity;	/* verbosity of logged messages */
 	int64		reject_limit;	/* maximum tolerable number of errors */
 	List	   *convert_select; /* list of column names (can be NIL) */
+	Node	   *routine;		/* CopyToRoutine or CopyFromRoutine (can be
+								 * NULL) */
 } CopyFormatOptions;
 
 /* These are private in commands/copy[from|to].c */
